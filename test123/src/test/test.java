0e4593f0f55a4b1a0991d75763df86676df04ccf
kpl123 <kapily@cybage.com>
2013-05-21 11:15:11 +0530

    12

--- test123/src/test/test.java
+++ test123/src/test/test.java
@@ -7,7 +7,7 @@ public class test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi.. i am kapil.....cybage.pune");
+		System.out.println("hi.. i am kapil yambal.....cybage.pune");
 	}
 
 }
